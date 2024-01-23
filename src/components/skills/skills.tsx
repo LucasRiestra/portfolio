eb449b8672866df9674c43a6d16521c7ab39363e
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-23 18:20:02 +0100

    updates2

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -7,25 +7,25 @@ const Skills = () => {
         <section className="skills-section">
             <h1 className="skills-title">Tech Skills</h1>
             <div className="skills-grid">
-                <img src="../src/skills-icons/html.svg" alt="Icon 1" className="skill-icon" />
-                <img src="../src/skills-icons/css.svg" alt="Icon 2" className="skill-icon" />
-                <img src="../src/skills-icons/javascript.svg" alt="Icon 3" className="skill-icon" />
-                <img src="../src/skills-icons/typescript.svg" alt="Icon 4" className="skill-icon" />
-                <img src="../src/skills-icons/react.svg" alt="Icon 5" className="skill-icon" />
-                <img src="../src/skills-icons/nodejs.svg" alt="Icon 6" className="skill-icon" />
-                <img src="../src/skills-icons/express.svg" alt="Icon 7" className="skill-icon" />
-                <img src="../src/skills-icons/mongodb.svg" alt="Icon 8" className="skill-icon" />
-                <img src="../src/skills-icons/github.svg" alt="Icon 9" className="skill-icon" />
-                <img src="../src/skills-icons/nextjs.svg" alt="Icon 10" className="skill-icon" />
-                <img src="../src/skills-icons/vercel.svg" alt="Icon 11" className="skill-icon" />
-                <img src="../src/skills-icons/railway.svg" alt="Icon 12" className="skill-icon" />
-                <img src="../src/skills-icons/jenkins.svg" alt="Icon 13" className="skill-icon" />
-                <img src="../src/skills-icons/postgre.png" alt="Icon 14" className="skill-icon" />
-                <img src="../src/skills-icons/cloudinary.svg" alt="Icon 15" className="skill-icon" />
-                <img src="../src/skills-icons/auth0.svg" alt="Icon 16" className="skill-icon" />
-                <img src="../src/skills-icons/bootstrap.svg" alt="Icon 17" className="skill-icon" />
-                <img src="../src/skills-icons/adobeillustrator.svg" alt="Icon 18" className="skill-icon" />
-                <img src="../src/skills-icons/vite.svg" alt="Icon 19" className="skill-icon" /> 
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029831/PortfolioIMGs/html_jxvklq.svg" alt="Icon 1" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029829/PortfolioIMGs/css_g0kzsj.svg" alt="Icon 2" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029831/PortfolioIMGs/javascript_k3d3wj.svg" alt="Icon 3" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029833/PortfolioIMGs/typescript_bissb9.svg" alt="Icon 4" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029833/PortfolioIMGs/react_rcb5ov.svg" alt="Icon 5" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029832/PortfolioIMGs/nodejs_mmguxo.svg" alt="Icon 6" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029829/PortfolioIMGs/express_p5qkll.svg" alt="Icon 7" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029831/PortfolioIMGs/mongodb_bib4vz.svg" alt="Icon 8" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029831/PortfolioIMGs/github_lntocx.svg" alt="Icon 9" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029832/PortfolioIMGs/nextjs_gekhtw.svg" alt="Icon 10" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029831/PortfolioIMGs/jenkins_mlevcq.svg" alt="Icon 11" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029829/PortfolioIMGs/cloudinary_df4wky.svg" alt="Icon 12" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029833/PortfolioIMGs/vercel_rdofit.svg" alt="Icon 13" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029833/PortfolioIMGs/postgre_ijige9.png" alt="Icon 14" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029833/PortfolioIMGs/railway_nmj8di.svg" alt="Icon 15" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029829/PortfolioIMGs/auth0_iueouv.svg" alt="Icon 16" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029829/PortfolioIMGs/bootstrap_mftua9.svg" alt="Icon 17" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029829/PortfolioIMGs/adobeillustrator_yujhy3.svg" alt="Icon 18" className="skill-icon" />
+                <img src="https://res.cloudinary.com/dtqgzojs3/image/upload/v1706029834/PortfolioIMGs/vite_eedjyp.svg" className="skill-icon" /> 
             </div>
         </section>
     )}
