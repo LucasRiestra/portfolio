f907ce2df0e38ee0b13afb3da77053c5578277df
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-23 20:03:09 +0100

    updates3

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,6 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            Thanks for watching
-            <br />
              © 2024 Portfolio Web by Lucas Riestra
         </footer>
     );
