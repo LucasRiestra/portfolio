48063d78f9444b06125d72cd9ab9b6a7b8c204e4
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-23 18:07:33 +0100

    update-portfolio

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -7,25 +7,25 @@ const Skills = () => {
         <section className="skills-section">
             <h1 className="skills-title">Tech Skills</h1>
             <div className="skills-grid">
-                <img src="../../../public/assets/icons/skills-icons/html.svg" alt="Icon 1" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/css.svg" alt="Icon 2" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/javascript.svg" alt="Icon 3" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/typescript.svg" alt="Icon 4" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/react.svg" alt="Icon 5" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/nodejs.svg" alt="Icon 6" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/express.svg" alt="Icon 7" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/mongodb.svg" alt="Icon 8" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/github.svg" alt="Icon 9" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/nextjs.svg" alt="Icon 10" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/vercel.svg" alt="Icon 11" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/railway.svg" alt="Icon 12" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/jenkins.svg" alt="Icon 13" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/postgre.png" alt="Icon 14" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/cloudinary.svg" alt="Icon 15" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/auth0.svg" alt="Icon 16" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/bootstrap.svg" alt="Icon 17" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/adobeillustrator.svg" alt="Icon 18" className="skill-icon" />
-                <img src="../../../public/assets/icons/skills-icons/vite.svg" alt="Icon 19" className="skill-icon" /> 
+                <img src="../src/skills-icons/html.svg" alt="Icon 1" className="skill-icon" />
+                <img src="../src/skills-icons/css.svg" alt="Icon 2" className="skill-icon" />
+                <img src="../src/skills-icons/javascript.svg" alt="Icon 3" className="skill-icon" />
+                <img src="../src/skills-icons/typescript.svg" alt="Icon 4" className="skill-icon" />
+                <img src="../src/skills-icons/react.svg" alt="Icon 5" className="skill-icon" />
+                <img src="../src/skills-icons/nodejs.svg" alt="Icon 6" className="skill-icon" />
+                <img src="../src/skills-icons/express.svg" alt="Icon 7" className="skill-icon" />
+                <img src="../src/skills-icons/mongodb.svg" alt="Icon 8" className="skill-icon" />
+                <img src="../src/skills-icons/github.svg" alt="Icon 9" className="skill-icon" />
+                <img src="../src/skills-icons/nextjs.svg" alt="Icon 10" className="skill-icon" />
+                <img src="../src/skills-icons/vercel.svg" alt="Icon 11" className="skill-icon" />
+                <img src="../src/skills-icons/railway.svg" alt="Icon 12" className="skill-icon" />
+                <img src="../src/skills-icons/jenkins.svg" alt="Icon 13" className="skill-icon" />
+                <img src="../src/skills-icons/postgre.png" alt="Icon 14" className="skill-icon" />
+                <img src="../src/skills-icons/cloudinary.svg" alt="Icon 15" className="skill-icon" />
+                <img src="../src/skills-icons/auth0.svg" alt="Icon 16" className="skill-icon" />
+                <img src="../src/skills-icons/bootstrap.svg" alt="Icon 17" className="skill-icon" />
+                <img src="../src/skills-icons/adobeillustrator.svg" alt="Icon 18" className="skill-icon" />
+                <img src="../src/skills-icons/vite.svg" alt="Icon 19" className="skill-icon" /> 
             </div>
         </section>
     )}
